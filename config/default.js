12a979df3caec692ebde0ed2ba4494e32a7a0115
iroskoshnyi <ivan.roskoshnyi@chromeriver.com>
2020-05-08 17:36:55 +0300

    send email with node mailer and juice

--- config/default.js
+++ config/default.js
@@ -1,10 +1,21 @@
 const defer = require('config/defer').deferConfig;
 const path = require('path');
 
+const {
+  email: user,
+  pass
+} = process.env;
+
 module.exports = {
   // secret data can be moved to env variables
   // or a separate config
   port: 3000,
+  nodemailer: {
+    gmail: {
+      user,
+      pass,
+    }
+  },
   knex: {
     dev: {
       connection: 'postgres://localhost:5433/koa_api',
@@ -13,6 +24,9 @@ module.exports = {
       connection: 'postgres://localhost:5433/koa_api_test',
     }
   },
+  static: defer(cfg => {
+    return path.join(cfg.root, 'src', 'server', 'static');
+  }),
   views: {
     // template.root uses config.root
     root: defer(function(cfg) {
--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,10 +2,48 @@ import Router from '@koa/router';
 import passport from 'koa-passport';
 import fs from 'fs';
 import queries from '../db/queries/users';
+
 import helpers from './_helpers';
 
+import nodemailer from 'nodemailer';
+
+import config from 'config';
+
+import juice from "juice";
+
 const router = new Router();
+
 router
+  .get('/mail', async ctx => {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: config.get('nodemailer').gmail
+    });
+    const templatePath = ctx.render('email');
+    const cssPath = config.get('static') + '/email.css';
+
+    const htmlFile = fs.readFileSync(templatePath, {encoding: 'utf8'});
+    const css = fs.readFileSync(cssPath, {encoding: 'utf8'});
+
+    const withStyles = juice.inlineContent(htmlFile, css);
+    const mailOptions = {
+      from: 'iroskoshnyi@softjourn.com', // sender address
+      to: "ivanroskishny@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: withStyles// html body
+    };
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+    ctx.body = 'ok';
+  })
   .get('/auth/register', async ctx => {
     ctx.type = 'html';
     ctx.body = fs.createReadStream(ctx.render('register'));
